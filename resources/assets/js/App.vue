055fe2e697f484a15d7353d8dbd1a86400bcf9ae
Adib Hanna <adibhanna@gmail.com>
2016-08-17 19:50:22 +0300

    add top nav

--- resources/assets/js/App.vue
+++ resources/assets/js/App.vue
@@ -1,67 +1,19 @@
 <template>
     <div>
-        <div class="leftsidebar">
-            <div class="logoarea">
-                <div class="videologo"></div>
-            </div>
+        <sidebar></sidebar>
 
-            <ul class="navlist">
-                <a v-link="'/dashboard'">
-                    <li v-link-active>DASHBOARD</li>
-                </a>
-                <a v-link="'/analytics'">
-                    <li class="">ANALYTICS</li>
-                </a>
-
-                <a v-link="'/campaigns'">
-                    <li class="">CAMPAIGNS</li>
-                </a>
-
-                <a v-link="'/create-campaigns'">
-                    <li class="">CREATE CAMPAIGN</li>
-                </a>
-
-                <a v-link="'/web-config'">
-                    <li class="">WEBSITE CONFIG</li>
-                </a>
-
-                <a v-link="'/support'">
-                    <li class="">SUPPORT</li>
-                </a>
-            </ul>
-        </div>
-
-        <!-- RIGHT SIDE -->
         <div class="rightside">
-            <div class="rightside-nav">
-                <div class="rightside-navlefttitle">
-
-                </div>
-
-                <!-- ACCOUNT DETAILS -->
-                <div id="accountdetails" class="rightside-navdropdown">ACCOUNT DETAILS <span></span></div>
-                <ul id="accountdetails-navdroparea" class="rightside-navdroparea" style="display:none;">
-                    <a href="/profile">
-                        <li>EDIT ACCOUNT</li>
-                    </a>
-                    <a href="/logout">
-                        <li>LOGOUT</li>
-                    </a>
-                </ul>
-
-                <!-- AVAILABLE WEBSITES -->
-                <div id="availablesites" class="rightside-navdropdown">WEBSITES <span></span></div>
-                <ul id="availablesites-navdroparea" class="rightside-navdroparea" style="display:none;">
-                    <a href="##">
-                        <li>SITE.COM</li>
-                    </a>
-                    <a href="##">
-                        <li>ABC.COM</li>
-                    </a>
-                </ul>
-            </div>
+            <rightside-nav></rightside-nav>
 
             <router-view></router-view>
         </div>
     </div>
 </template>
+<script>
+    import Sidebar from './components/Sidebar.vue'
+    import RightsideNav from './components/RightsideNav.vue'
+
+    export default {
+        components: { Sidebar, RightsideNav }
+    }
+</script>
---
+++ resources/assets/js/components/Sidebar.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <div class="leftsidebar">
+            <div class="logoarea">
+                <div class="videologo"></div>
+            </div>
+
+            <ul class="navlist">
+                <a v-link="'/dashboard'">
+                    <li v-link-active>DASHBOARD</li>
+                </a>
+                <a v-link="'/analytics'">
+                    <li class="">ANALYTICS</li>
+                </a>
+
+                <a v-link="'/campaigns'">
+                    <li class="">CAMPAIGNS</li>
+                </a>
+
+                <a v-link="'/create-campaigns'">
+                    <li class="">CREATE CAMPAIGN</li>
+                </a>
+
+                <a v-link="'/web-config'">
+                    <li class="">WEBSITE CONFIG</li>
+                </a>
+
+                <a v-link="'/support'">
+                    <li class="">SUPPORT</li>
+                </a>
+            </ul>
+        </div>
+    </div>
+</template>
