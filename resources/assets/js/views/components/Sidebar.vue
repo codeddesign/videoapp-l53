48ef09435d539daddb2dc7aaabab302a964a7420
Adib Hanna <adibhanna@gmail.com>
2016-08-25 13:47:32 +0300

    styling fixes

--- resources/assets/js/views/components/Sidebar.vue
+++ resources/assets/js/views/components/Sidebar.vue
@@ -8,37 +8,37 @@
             <ul class="navlist">
                 <li v-link="{name:'dashboard'}">
                     <a>
-                        <i class="fa fa-dashboard"></i> DASHBOARD
+                        <i class="fa fa-dashboard"></i> <span>DASHBOARD</span>
                     </a>
                 </li>
 
                 <li v-link="{name:'campaigns.listing'}">
                     <a>
-                        <i class="fa fa-newspaper-o"></i> CAMPAIGNS
+                        <i class="fa fa-newspaper-o"></i> <span>CAMPAIGNS</span>
                     </a>
                 </li>
 
                 <li v-link="{name:'campaigns.create'}">
                     <a>
-                        <i class="fa fa-plus"></i> CREATE CAMPAIGN
+                        <i class="fa fa-plus"></i> <span>CREATE CAMPAIGN</span>
                     </a>
                 </li>
 
                 <li v-link="{name:'analytics'}">
                     <a>
-                        <i class="fa fa-bar-chart"></i> ANALYTICS
+                        <i class="fa fa-bar-chart"></i> <span>ANALYTICS</span>
                     </a>
                 </li>
 
                 <li v-link="{name:'webconfig'}">
                     <a>
-                        <i class="fa fa-globe"></i> WEBSITES
+                        <i class="fa fa-globe"></i> <span>WEBSITES</span>
                     </a>
                 </li>
 
                 <li v-link="{name:'support'}">
                     <a>
-                        <i class="fa fa-flag"></i> SUPPORT
+                        <i class="fa fa-flag"></i> <span>SUPPORT</span>
                     </a>
                 </li>
             </ul>
