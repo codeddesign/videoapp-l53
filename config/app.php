72beba136a13c5c115bb87d5ee7389b0f2ec1787
Rui Gomes <hello@ruigomes.me>
2016-10-17 16:12:39 +0100

    Remove unneeded package

--- config/app.php
+++ config/app.php
@@ -177,8 +177,6 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Laravel\Passport\PassportServiceProvider::class,
-        Vinelab\Api\ApiServiceProvider::class,
-
         App\CampaignEvents\Providers\CampaignEventsServiceProvider::class,
 
         // Sentry integration
