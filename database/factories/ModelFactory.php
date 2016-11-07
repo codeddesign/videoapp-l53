d75655d4fe80ba1f598d050470930545bf04b441
Rui Gomes <hello@ruigomes.me>
2016-11-07 17:23:00 +0000

    Fix User Factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -19,7 +19,7 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'email' => $faker->safeEmail,
         'verified_email' => 1,
         'verified_phone' => 1,
-        'password' => bcrypt('123456'),
+        'password' => '123123',
         'remember_token' => str_random(10),
     ];
 });
