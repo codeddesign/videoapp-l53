d9811d37b6bcf32edad485ab4fb4c6eeb592417e
Rui Gomes <hello@ruigomes.me>
2017-01-08 15:27:19 +0000

    Change URL to text type

---
+++ database/migrations/2017_01_08_150226_change_tags_url_type.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeTagsUrlType extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        \DB::statement('ALTER TABLE tags ALTER COLUMN url TYPE TEXT');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        \DB::statement('ALTER TABLE tags ALTER COLUMN url TYPE VARCHAR');
+    }
+}
