6151205099edac7e2149cccde04815c598420d4b
Rui Gomes <hello@ruigomes.me>
2017-01-31 13:32:49 +0000

    Prevent tests running in wrong DB

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -10,12 +10,14 @@ abstract class DuskTestCase extends BaseTestCase
 {
     use CreatesApplication;
 
-    /**
-     * The list of database tables.
-     *
-     * @var array
-     */
-    protected static $tables = [];
+    protected function setUp()
+    {
+        parent::setUp();
+
+        if (strpos(app('db')->connection()->getDatabaseName(), 'testing') === false) {
+            die("Tests should be ran on properly configured development machines only.\n");
+        }
+    }
 
     /**
      * Prepare for Dusk test execution.
@@ -26,31 +28,6 @@ abstract class DuskTestCase extends BaseTestCase
     public static function prepare()
     {
         static::startChromeDriver();
-    }
-
-    public function prepareDb()
-    {
-        if (defined('TESTS_DB_PREPARED')) {
-            return;
-        }
-
-        define('TESTS_DB_PREPARED', true);
-
-        /** @var DatabaseManager $db */
-        $db = $this->app['db'];
-
-        if (strpos($db->connection()->getDatabaseName(), 'testing') === false) {
-            die("Tests should be ran on properly configured development machines only.\n");
-        }
-
-        if ($this->app['migrator']->repositoryExists()) {
-            $db->statement('DROP SCHEMA public CASCADE');
-            $db->statement('CREATE SCHEMA public');
-        }
-
-        $this->artisan('migrate');
-
-        static::$tables = $db->table('information_schema.tables')->where('table_schema', 'public')->get()->pluck('table_name');
     }
 
     /**
