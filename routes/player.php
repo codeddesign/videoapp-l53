199d8227645abc8aedab25a4cf41ef2fe56328c8
Rui Gomes <hello@ruigomes.me>
2017-02-01 14:59:25 +0000

    Fully remove PHP tracking

--- routes/player.php
+++ routes/player.php
@@ -2,5 +2,4 @@
 
 // Public endpoint, used by the player.
 Route::get('/campaign/{id}', 'CampaignsController@campaign');
-Route::get('/track', 'TrackController@index');
 Route::get('/plugin', 'PluginController@CampaignAdd');
