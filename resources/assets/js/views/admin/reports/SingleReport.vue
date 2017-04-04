81ccb3d47ca275201ca80755b70271de558e69db
Rui Gomes <hello@ruigomes.me>
2017-04-04 11:04:16 +0100

    Add % sign to reports

--- resources/assets/js/views/admin/reports/SingleReport.vue
+++ resources/assets/js/views/admin/reports/SingleReport.vue
@@ -135,7 +135,7 @@
         <ul class="dashboard-dailystatslist dashreports-width">
           <li v-for="stats in paginatedStats">
             <div :class="'dashboard-statslist' + (index === 0 ? '1' : '2')" v-for="(stat, key, index) in stats">
-              <span v-if="['ecpm', 'revenue'].indexOf(key) !== -1">$</span>{{ stat }}
+              <span v-if="['ecpm', 'revenue'].indexOf(key) !== -1">$</span>{{ stat }}<span style="margin-left:0;" v-if="['fill_rate', 'error_rate', 'ctr', 'completion_rate'].indexOf(key) !== -1">%</span>
             </div>
           </li>
         </ul>
