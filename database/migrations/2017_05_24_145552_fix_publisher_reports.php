1f40847038ba2e487ed4ebba3c44fab26a95e430
Rui Gomes <hello@ruigomes.me>
2017-05-24 18:19:11 +0100

    Remove data from publishers reports

---
+++ database/migrations/2017_05_24_145552_fix_publisher_reports.php
@@ -0,0 +1,48 @@
+<?php
+
+use App\Models\Report;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class FixPublisherReports extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $reports = DB::table('reports')->get();
+        $users   = DB::table('users')->get();
+
+        $reportsToUpdate = [];
+
+        foreach ($reports as $report) {
+            $user    = $users->where('id', $report->user_id)->first();
+            $isAdmin = $user->admin;
+
+            if (! $isAdmin) {
+                $reportsToUpdate[] = $report->id;
+            }
+        }
+
+        DB::table('reports')->whereIn('id', $reportsToUpdate)->update([
+            'combine_by'       => 'website',
+            'sort_by'          => 'website',
+            'included_metrics' => json_encode(Report::allUserMetrics()),
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
